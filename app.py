e4a9f98e802164b3e9e7874cfe9fbc62b83cddb7
senKo183 <lehungnlb@gmail.com>
2025-03-07 17:12:31 +0700

    Add Login and Registration both in frontend and backend

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'adc184bf5a300e53354d8bf219d6a19d' 
 
 @app.route("/")
 @app.route("/home")
-def hello_world():
+def home():
     return render_template('home.html')
 
-@app.route("/login")
-def About():    
-    return render_template('login.html')
-if __name__ =='__main__':
+@app.route("/login", methods=['GET', 'POST'])
+def login():   
+    form = LoginForm() 
+    if form.validate_on_submit():
+        if form.username.data == 'admin' and form.password.data == 'password':
+            flash(f'Login successful for {form.username.data}!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+if __name__ == '__main__':
     app.run(debug=True)
 
